5c47336c661a8f2a83b9843cc6d04bd4996f0fc6
Ramalingam <ramalingam2109@outlook.com>
2025-07-05 21:37:35 +0530

    completed backend and tested in postman

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config'
 import cookieParser from 'cookie-parser'
 import connectMongoDB from './config/mongodb.js'
 import { authRouter } from './routes/authRoutes.js'
+import userRouter from './routes/userRoutes.js'
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -20,3 +21,4 @@ app.get('/', (req, res) => {
   res.send('API Working ')
 })
 app.use('/api/auth', authRouter)
+app.use('/api/user', userRouter)
